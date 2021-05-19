a9a82386146ec9b5fc42b520f5c0413b432c6c91
ElinaMishra <77671824+ElinaMishra@users.noreply.github.com>
2021-05-19 15:14:35 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,21 @@
+class launcher{
+    constructor(bodyA,bodyB){
+        var options ={
+            bodyA : bodyA,
+            bodyB : bodyB,
+            stiffness : 0.04,
+            length:10
+        }
+        this.launcher = Constraint.Create(options);
+        World.add(world.this.launcher);
+    }
+    fly(){
+        this.launcher.bodyA=null;
+    }
+    display(){
+        var pointA = this.launcher.bodyA.position;
+        var pointB = this.launcher.bodyB.position;
+        strokeWeight(4);
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+}
